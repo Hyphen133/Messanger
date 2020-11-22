1afb93da08ce011ff07d990d88cd76450a066d9c
Hyphen133 <hyphen133@gmail.com>
2020-11-22 11:46:53 +0100

    CI test check

--- messaging-api/src/test/java/application/MessagingAPIReadMessagesScenariosTests.java
+++ messaging-api/src/test/java/application/MessagingAPIReadMessagesScenariosTests.java
@@ -23,6 +23,7 @@ public class MessagingAPIReadMessagesScenariosTests {
 
         //Then
         Assert.assertEquals(4, messagesForChat.size());
+        Assert.assertFalse(true);
     }
 
     private Chat createChat() {
