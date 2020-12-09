06e69ef5702faf7a45f8e206a3c881c8a88c4147
Hyphen133 <hyphen133@gmail.com>
2020-12-09 14:54:12 +0100

    Local messaging agent working

--- messaging-api/src/test/java/performance/single_test.js
+++ messaging-api/src/test/java/performance/single_test.js
@@ -1,16 +1,10 @@
 const WebSocket = require('ws');
-const fs = require('fs');
 
 var user = process.argv[2]
 var chatId = process.argv[3]
 var num_messages = parseInt(process.argv[4])
 var sleep_time_ms = parseInt(process.argv[5])
-var log_path = process.argv[6]
 
-
-
-let writeStream = fs.createWriteStream(log_path);
-writeStream.write("TEST for " + user + " and chat " + chatId +"\n")
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -34,14 +28,14 @@ async function connection(socket, timeout = 10000) {
 }
 
 function getMessage(type, content){
-    return "[" + new Date().toDateString() + "]" + type +": " + content + " chat: " + chatId + "\n"
+    return "[" + new Date().toLocaleString() + "]" + type +": " + content + " chat: " + chatId
 }
 
 async function test(number_of_messages, break_between_messages, filestream) {
     const websocket = new WebSocket('ws://localhost:8080/webSocket/' + user)
 
     websocket.on('message', function incoming(data) {
-        console.log(getMessage("Recieved", ""))
+        console.log(getMessage("Received", ""))
     });
 
     const opened = await connection(websocket)
@@ -57,20 +51,15 @@ async function test(number_of_messages, break_between_messages, filestream) {
 
             let message = getMessage("Sent", content)
             console.log(message)
-            // try {
-            //     writeStream.write(message)
-            // }catch (e){
-            //     console.log(e);
-            // }
+
             await sleep(sleep_time_ms);
         }
 
     } else {
-        writeStream.write("ERROR on Opening Connection")
+        console.log("ERROR on Opening Connection")
         return
     }
     websocket.close()
 }
 
 test(num_messages, sleep_time_ms)
-writeStream.close()
