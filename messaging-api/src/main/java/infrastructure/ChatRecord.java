54071988a54ae10a8c8f75c6341799bb614abad6
Hyphen133 <hyphen133@gmail.com>
2020-12-13 12:01:46 +0100

    Factories

--- messaging-api/src/main/java/infrastructure/ChatRecord.java
+++ messaging-api/src/main/java/infrastructure/ChatRecord.java
@@ -2,9 +2,11 @@ package infrastructure;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "chats")
 public class ChatRecord {
     @Id
     public UUID id;
--- messaging-api/src/main/java/infrastructure/MessageRecord.java
+++ messaging-api/src/main/java/infrastructure/MessageRecord.java
@@ -5,9 +5,11 @@ import domain.ChatMessage;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "messages")
 public class MessageRecord {
     @Id @GeneratedValue
     public Long id;
